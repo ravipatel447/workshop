c24cd2e0ac97b7046f97497d9cdf19075d49c5ae
ravipatel447 <patelravi44741@gmail.com>
2024-03-10 22:19:52 +0530

    feat(Error handling): :sparkles: added error handling and common response functions

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const config = require("./config/config");
+const { errorController } = require("./controllers");
 const routes = require("./routes");
+const ApiError = require("./utils/ApiError");
+const httpStatus = require("http-status");
+const catchAsync = require("./utils/catchAsync");
+const { tokenMessages } = require("./messages");
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1", routes);
+// error handler
+app.all(
+  "*",
+  catchAsync(async (req, res) => {
+    throw new ApiError(
+      tokenMessages.error.PAGE_NOT_FOUND,
+      httpStatus.NOT_FOUND
+    );
+  })
+);
+app.use(errorController);
+
 const port = config.system.port;
