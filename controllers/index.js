0736d0fbd44b4cc04df7a47ffdf980ec29c8d42c
ravipatel447 <patelravi44741@gmail.com>
2024-03-10 23:10:50 +0530

    feat(post): :sparkles: added post API's with related to user

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
 module.exports.authController = require("./auth.controller");
 module.exports.errorController = require("./error.controller");
 module.exports.userController = require("./user.controller");
+module.exports.postController = require("./post.controller");
--- messages/index.js
+++ messages/index.js
@@ -1,2 +1,3 @@
 module.exports.tokenMessages = require("./token.messages");
 module.exports.userMessages = require("./user.messages");
+module.exports.postMessages = require("./post.messages");
