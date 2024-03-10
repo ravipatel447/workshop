6e2fcad0c66a5f0bd43eb110414d3073f9d84500
ravipatel447 <patelravi44741@gmail.com>
2024-03-10 22:02:56 +0530

    refactor(hello): :fire: removed hello api

--- routes/hello.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers");
-
-router.get("/hello", controller.hello.helloToUsers);
-
-module.exports = router;
